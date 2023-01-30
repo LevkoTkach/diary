d02b1391d070f0f1dc6277d70b7d5c2cb6cd34ea
Levko Tkach <91378152+LevkoTkach@users.noreply.github.com>
2023-01-30 12:42:50 +0200

    add route to NoteList page

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import NoteListPage from './pages/NoteListPage';
 
 setupIonicReact();
 
@@ -37,7 +38,8 @@ const App: React.FC = () => {
             <Route path="/page/:name" exact={true}>
               <Page />
             </Route>
-            <Route path="/text/:date" exact={true} component={NotePage} />
+            <Route path="/note/:date" exact={true} component={NotePage} />
+            <Route path="/note-list/:date" exact={true} component={NoteListPage} />
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
