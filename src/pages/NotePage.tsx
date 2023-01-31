2a03b79288987cf934f675878cdfb012b426df00
Levko Tkach <91378152+LevkoTkach@users.noreply.github.com>
2023-01-31 23:27:00 +0200

    finish styles note page

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Component } from "react";
-import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonLabel, IonMenuButton, IonPage, IonRadio, IonRadioGroup, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonLoading, IonPage, IonRadio, IonRadioGroup, IonTextarea, IonTitle, IonToolbar } from "@ionic/react";
 import TextEditor from "../components/TextEditor";
 import { RouteComponentProps } from "react-router";
-
+import { arrowBackOutline } from "ionicons/icons";
+import './NotePage.css';
 
 interface AddProps {
   date: string;
 }
 
 interface AddStates {
-
+  date: string;
 }
 
 class NotePage extends Component<RouteComponentProps<AddProps>> {
@@ -19,45 +20,45 @@ class NotePage extends Component<RouteComponentProps<AddProps>> {
     const save = (newValue: string) => {
       console.log('save', newValue);
     }
-    const titleDate: string = this.props.match.params.date + ' notaition';
+    const titleDate: string = this.props.match.params.date;
 
     return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButton>
-              <IonIcon>kjbilb</IonIcon>
-              Back
-            </IonButton>
-            <IonTitle>
-              {titleDate}
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonPage className="page">
+        <IonHeader className="ion-no-border header">
 
+          <IonButton shape="round" fill="clear" className="back-button">
+            <IonIcon className="arrow-icon" slot="start" icon={arrowBackOutline} ></IonIcon>
+            Back
+          </IonButton>
+          <IonButton fill="clear" className="title-date">
+            <IonLabel>{titleDate}</IonLabel>
+          </IonButton>
+          <IonButton shape="round" fill="clear" className="save-button">
+            Save
+          </IonButton>
+        </IonHeader>
         <IonContent fullscreen>
-          <TextEditor
-            value={"title"}
-            onChange={save}
-          />
-          <TextEditor
-            value={"note"}
-            onChange={save}
-          />
-        </IonContent>
-        <IonToolbar>
-          <IonLabel>
+          <IonTextarea
+            className="title-textarea ion-no-padding"
+            placeholder="  Title"
+          ></IonTextarea>
+          <IonTextarea
+            className="custom-textarea "
+            placeholder="Write your message in here.."
+          ></IonTextarea>
+          <IonLabel className="color_label">
+            Choose a color
           </IonLabel>
-          <IonRadioGroup >
-            <IonRadio></IonRadio>
-            <IonRadio></IonRadio>
-            <IonRadio></IonRadio>
-            <IonRadio></IonRadio>
-            <IonRadio></IonRadio>
-            <IonRadio></IonRadio>
+          <IonRadioGroup className="radio-group" value="custom-checked">
+            <IonRadio className="green-radio" value="custom-checked"></IonRadio>
+            <IonRadio className="blue-radio"></IonRadio>
+            <IonRadio className="purple-radio"></IonRadio>
+            <IonRadio className="red-radio"></IonRadio>
+            <IonRadio className="yellow-radio"></IonRadio>
+            <IonRadio className="brown-radio"></IonRadio>
           </IonRadioGroup>
-        </IonToolbar>
-        <IonButton>done</IonButton>
+        </IonContent>
+        <IonButton>Gogle Ads</IonButton>
       </IonPage>
     );
   }
