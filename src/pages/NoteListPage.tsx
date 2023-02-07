fef9f8a50dca53fe6f3d3c7cf6052adfd5e95181
Levko Tkach <91378152+LevkoTkach@users.noreply.github.com>
2023-02-08 00:36:17 +0200

    ad style to none list

--- src/pages/NoteListPage.tsx
+++ src/pages/NoteListPage.tsx
@@ -1,48 +1,76 @@
+
 import React from "react";
-import { IonButton,  IonCol, IonContent, IonHeader, IonIcon,  IonPage,  IonText, IonTitle, IonToolbar } from "@ionic/react";
+
+import { IonButton, IonHeader, IonIcon, IonLabel, IonPage, IonRadio, IonRadioGroup, IonToolbar } from "@ionic/react";
+import { arrowBackOutline, chevronBackOutline, chevronForwardOutline, pencil, pencilSharp } from "ionicons/icons";
+import './NoteListPage.css';
 import { useParams } from "react-router";
+import NoteCard from "../components/NoteCadr";
 
 
+// const store = new Storage();
+// await store.create();
+interface addProps {
+  date: string;
+}
 
-const NoteListPage: React.FC = () => {
+const NoteListPage: React.FC<addProps> = () => {
   const { date } = useParams<{ date: string; }>();
+  const titleDate: string = date;
 
-      const save = (newValue: string) => {
-      console.log('save', newValue);
+
+  const setting = (id: string) => {
+    return localStorage.getItem(id + date)!;
   }
-  
-    const titleDate: string = date + ' notaition';
+  const getTitle = setting('title');
+  const getNote = setting('note');
 
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButton>
-              <IonIcon></IonIcon>
-              Back
-            </IonButton>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent >
-          <IonTitle>
-            {titleDate}
-          </IonTitle>
-
-          <IonButton>
-            <IonCol>
-              <IonTitle>Title</IonTitle>
-              <IonText> note text </IonText>
-            </IonCol>
-            <IonIcon>
-              icon
-            </IonIcon>
+  return (
+    <IonPage >
+      <IonPage className="page">
+        <IonHeader className="ion-no-border header">
+          <IonButton
+            shape="round"
+            fill="clear"
+            className="back-button">
+            <IonIcon className="arrow-icon" slot="start" icon={arrowBackOutline} ></IonIcon>
+            Back
           </IonButton>
+      
+            <IonButton
+              shape="round"
+              fill="clear"
+              className="back-date-button">
+              <IonIcon className="arrow-icon " slot="start" icon={chevronBackOutline} ></IonIcon>
+            </IonButton>
+            <IonLabel className="title-date_list-page">{titleDate}</IonLabel>
+            <IonButton
+              shape="round"
+              fill="clear"
+              className="forward-date-button">
+              <IonIcon className="arrow-icon" slot="end" icon={chevronForwardOutline} >
+              </IonIcon>
+            </IonButton>          
 
-        </IonContent>
-        <IonButton>done</IonButton>
+        </IonHeader>
+        <NoteCard className="note-card" />
+        <IonButton
+          className="compose-button"
+          shape="round">
+          <IonIcon
+            className="pen-icon"
+            slot="start"
+            icon={pencilSharp}>
+          </IonIcon>
+          Compose
+        </IonButton>
       </IonPage>
-    );
-  }
+      <IonButton className="google-ads-area" >Google Ads</IonButton>
+    </IonPage>
+  );
+}
 
 
 export default NoteListPage;
+
+
