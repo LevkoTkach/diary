d03f5443841f630dc661af2a8b724c3700225c27
Levko Tkach <91378152+LevkoTkach@users.noreply.github.com>
2023-02-10 17:54:16 +0200

    corecded import card

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -3,7 +3,7 @@ import { FormEvent, FormEventHandler } from 'react';
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon, } from '@ionic/react';
 import { RouteComponentProps, useParams } from 'react-router';
 import { arrowForwardOutline } from 'ionicons/icons';
-import './NoteCadr.css';
+import './NoteCard.css';
 
 interface addProps {
   className: string;
